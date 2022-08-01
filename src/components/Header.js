b6f581ac29f94e0988a929be382560b47eac9855
MosNes <7hekarl@gmail.com>
2022-08-01 14:41:53 -0400

    added task.js and button.js added seed tasks.json

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
 const Header = (props) => {
+    //define functions for props inside
+    const onClick = () => {
+        console.log('Click')
+    }
+
   return (
-    <header>
+    <header className='header'>
         <h1>{props.title}</h1>
+        <Button color='green' text='Add' onClick={onClick} />
     </header>
   )
 }
@@ -10,5 +19,17 @@ const Header = (props) => {
 Header.defaultProps = {
     title: 'Task Tracker'
 }
+
+//uses PropTypes to specify types for each of the props used by the component
+//not needed but helps prevents errors
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+//CSS in JS, pass headingStyle into element style property, e.g. style={{headingStyle}} for in-line styling
+// const headingStyle = {
+//     color: 'black',
+//     backgroundColor: 'black'
+// }
 
 export default Header
